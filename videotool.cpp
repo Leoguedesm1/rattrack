6708993e95a3d4e6b4ec8a565c2014e9484bd68b
Leonardo Guedes Moreira <leo_greengm@hotmail.com>
2016-08-31 20:57:30 -0300

    Cutting and show 2 images

--- videotool.cpp
+++ videotool.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 using namespace cv;
 
@@ -14,7 +15,7 @@ int threshold_value = 0; //Valor do Threshold
 int const max_value = 255; //Valor máximo da Trackbar
 int const max_BINARY_value = 255;
 
-Mat src, out; //Imagens de entrada e saída
+Mat src, out, tela; //Imagens de entrada e saída
 char* window_name = "VideoTool"; 
 char* trackbar_value = "Value";
 
@@ -49,36 +50,45 @@ int main( int argc, char** argv ) {
 
 void Threshold( int, void* ) {
   /*Coordenadas da imagem*/
-  int Xi=402, Yi=206; //Iniciais
-  int X=966, Y=604; //Finais
+  int left=350, top=200, width=390, height=360;
 
-  /*Auxiliares*/
-  int i, j;
+  /*Criando a imagem de saida*/
+  out = Mat(src.rows, src.cols, src.type());
+	
+  /*Criando imagem da tela*/
+  tela = Mat(src.rows, src.cols+out.cols, src.type());
 
-  /*Atribuindo os valores de 0 para toda matriz out*/
-  for(i = 0; i < src.rows; i++) {
+  /*Atribuindo os valores de para matriz out*/
+  for(int i = 0; i < src.rows; i++) {
 
-    for(j = 0; j < src.cols; j++) {
+    for(int j = 0; j < src.cols; j++) {
      
       /*Area of interest*/
-      if((i >= Xi && i <= X) && (j >= Yi && j <= Y))
+      /*Coordenadas: início +- (402, 206) - (966, 604)*/
+      if((i >= top && i <= top+width-1) && (j >= left && j <= left+height-1))
 	out.at<int>(i, j) = src.at<int>(i, j);
       else
        out.at<int>(i, j) = 0;
     }
   }
 
+  /*Copiando as imagens para a saída da tela*/
+  src.copyTo(tela(Rect( 0, 0, src.cols, src.rows)));
+  out.copyTo(tela(Rect(src.cols, 0, out.cols, out.rows)));
+  Size size(1200, 550);
+  resize(tela, tela, size);
+
   /*Threshold
   for(i = Xi; i <= X; i++) {
     for(j = Yi; j <= Y; j++) {
-      if (src.at(i, j) > threshold_value) {
+      if (src.at<int>(i, j) > threshold_value) {
         out.at<int>(i, j) = WHITE;
       }else{
       	out.at<int>(i, j) = BLACK;
       }
     }
   }*/
-
-  imshow( window_name, out );
-}
+ 
+  imshow( window_name, tela);
+ }
 
