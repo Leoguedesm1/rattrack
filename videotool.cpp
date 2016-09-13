3c6973168e8057b803ef9e21aaeb8f4d1f7ba41c
Leonardo Guedes Moreira <leo_greengm@hotmail.com>
2016-09-12 21:49:09 -0300

    Tracking test

--- videotool.cpp
+++ videotool.cpp
@@ -1,25 +1,29 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 /*Definindo valores de pixels BRANCO e PRETO*/
 #define WHITE 255
 #define BLACK 0 
 
 /*Variáveis Globais*/
-int threshold_value = 128; //Valor do Threshold
+int threshold_value = 137; //Valor do Threshold
 int const max_value = 255; //Valor máximo da Trackbar
+RNG rng(12345);
 
 /*Coordenadas da imagem*/
 /*Coordenadas: início +- (402, 206) - (966, 604)*/
 // Imagen 704x480
-int left=220, top=120, width=300, height=240;
+int left1=220, top=120, width=300, height=240;
 
 Mat out, tela; //Imagens de entrada e saída
 
 /*Assinatura das funções*/
 void Threshold( int, void* );
+void tracking(Mat aux);
 
 int main( int argc, char** argv ) {
 
@@ -30,7 +34,7 @@ int main( int argc, char** argv ) {
   namedWindow( "VideoTool" , CV_WINDOW_AUTOSIZE );
 
   /*Atribuindo os valores de para matriz out usando ROI*/
-  out = src(Rect(left, top, width, height));
+  out = src(Rect(left1, top, width, height));
   
   /*Criando imagem da tela*/
   tela = Mat(src.rows, src.cols+src.cols, CV_8U, Scalar(0));
@@ -69,10 +73,38 @@ void Threshold( int, void* ) {
         aux.at<uchar>(i, j) = WHITE;
     }
   }
-  medianBlur(aux, aux, 5);
-  /*Copiando a imagen corrigida por ROI*/
-  aux.copyTo(tela(Rect(left+tela.cols/2, top, aux.cols, aux.rows)));
   
-  imshow( "VideoTool", tela);
+  medianBlur(aux, aux, 5);
+  
+  tracking(aux);
 
+}
+
+void tracking(Mat aux) {
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	
+	findContours( aux, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+  	
+  	/// Get the moments
+  	vector<Moments> mu(contours.size() );
+  	//for( int i = 0; i < contours.size(); i++ )
+  		mu[0] = moments( contours[0], false ); 
+
+	///  Get the mass centers:
+ 	vector<Point2f> mc( contours.size() );
+  	//for( int i = 0; i < contours.size(); i++ ) {
+  		mc[0] = Point2f( mu[0].m10/mu[0].m00 , mu[0].m01/mu[0].m00 ); 
+  	//}
+  
+ 	//for( int i = 0; i< contours.size(); i++ ) {
+  		Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+    	//drawContours( aux, contours, i, color, 2, 8, hierarchy, 0, Point() );
+    	circle( aux, mc[0], 4, color, -1, 8, 0 );
+  	//}
+  	
+	/*Copiando a imagen corrigida por ROI*/
+    aux.copyTo(tela(Rect(left1+tela.cols/2, top, aux.cols, aux.rows)));
+  
+	imshow( "VideoTool", tela);
 }
