4e292cfe4720f2672f17b5a7f8fd59b7fdfd14f7
Leonardo Guedes Moreira <leo_greengm@hotmail.com>
2016-10-10 21:06:52 -0300

    Perspective and circle!

--- videotool.cpp
+++ videotool.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <algorithm>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/opencv.hpp"
 #include <iostream>
+
+#include <limits>
+
+const double INF = std::numeric_limits<double>::infinity();
 
 using namespace cv;
 using namespace std;
@@ -16,39 +22,56 @@ using namespace std;
 // Imagen 704x480
 int left1=220, top=120, width=300, height=240;
 Mat src_frame, out, tela;
-int threshold_value = 110; //Valor do Threshold
+int threshold_value = 130; //Valor do Threshold
 int const max_value = 255; //Valor máximo da Trackbar
 
-int min_area = 100;
-int max_area = 400;
-int const max_area_value = 1000;
+VideoCapture src;
+
+/*Area Bar*/
+int min_area = 300;
+int max_area = 1300;
+int const max_area_value = 1500;
+
+/*Reset Bar*/
+int reset = 0;
+int const max_reset = 1;
+
+/*teste*/
+vector<Point2f> centerTrans;
 
 /*Assinatura das funções*/
 void Threshold( int, void* );
 void tracking(Mat aux);
-void callbackButton( int, void* );
+void Reset( int, void* );
 
 int main(int argc, char** argv) {
 	/*Capturando video*/
-    VideoCapture src(argv[1]);
-	src.set(CAP_PROP_POS_MSEC, 360000);
+    src.open(argv[1]);
+	src.set(CAP_PROP_POS_MSEC, 380000);
 	
 	/*Verificando se o video foi aberto*/
     if( !src.isOpened() )
         throw "Error when reading steam_avi";
                  
 	/*Janela*/
-    namedWindow( "VideoTool", CV_WINDOW_AUTOSIZE );  	
+    namedWindow( "VideoTool", WINDOW_NORMAL );  	
+    resizeWindow( "VideoTool", 704, 480);
+    //namedWindow( "VideoTool2", WINDOW_NORMAL );  	
+    //resizeWindow( "VideoTool2", 704, 480);
+    namedWindow( "Trackbars", WINDOW_NORMAL);
     
+    /*Trackbar reseta o video*/
+    createTrackbar( "Reset", "Trackbars", &reset, max_reset, Reset );
  	/*Criando a Trackbar para alterar o valor do Thresholding*/
-    createTrackbar( "Value", "VideoTool", &threshold_value, max_value, Threshold );
-    
+    createTrackbar( "Threshold", "Trackbars", &threshold_value, max_value, Threshold );
     /*Trackbars para a area do rato*/
-    createTrackbar( "Min", "VideoTool", &min_area, max_area_value, Threshold );
-  	createTrackbar( "Max", "VideoTool", &max_area, max_area_value, Threshold );
+    createTrackbar( "Area Min", "Trackbars", &min_area, max_area_value, Threshold );
+  	createTrackbar( "Area Max", "Trackbars", &max_area, max_area_value, Threshold );
     
-  	tela = Mat(src.get(CV_CAP_PROP_FRAME_HEIGHT), src.get(CV_CAP_PROP_FRAME_WIDTH)*2, CV_8U, Scalar(0));
-    
+ 	//tela = Mat(src.get(CV_CAP_PROP_FRAME_HEIGHT), src.get(CV_CAP_PROP_FRAME_WIDTH)*2, CV_8U, Scalar(0));
+ 	tela = Mat(src.get(CV_CAP_PROP_FRAME_HEIGHT), src.get(CV_CAP_PROP_FRAME_WIDTH)*2, src_frame.type());
+ 	Reset (0, 0);
+   
     /*Loop*/
     for( ; ; )
     {
@@ -57,32 +80,92 @@ int main(int argc, char** argv) {
     	if(src_frame.empty())
             break;
         
-        Mat src_gray, aux_gray;
+        Mat src_gray, transform = Mat::zeros( src_gray.rows, src_gray.cols, CV_8UC3 );
+        
+        /*Passando frame do vídeo para GrayScale*/
         cvtColor(src_frame, src_gray, cv::COLOR_RGB2GRAY);
-        cvtColor(src_frame, aux_gray, cv::COLOR_RGB2GRAY);
-  		/*Atribuindo os valores de para matriz out usando ROI*/
-  		out = src_gray(Rect(left1, top, width, height));
-   		
-   		/*Affine Transformation*/
-   		Point2f inputQuad[4];
-   		Point2f outputQuad[4];
-   		Mat lambda( 2, 4, CV_32FC1 );
-   		lambda = Mat::zeros( src_gray.rows, src_gray.cols, src_gray.type() );
-   		
-   		inputQuad[0] = Point2f( left1, top );
-    	inputQuad[1] = Point2f( left1+width, top);
-    	inputQuad[2] = Point2f( left1+width, top+height);
-    	inputQuad[3] = Point2f( left1, top+height );
-    	outputQuad[0] = Point2f( 0, 0 );
-   		outputQuad[1] = Point2f( src_gray.cols-1,0);
-    	outputQuad[2] = Point2f( src_gray.cols-1,src_gray.rows-1);
-    	outputQuad[3] = Point2f( 0,src_gray.rows-1  );
- 
- 		lambda = getPerspectiveTransform( inputQuad, outputQuad );
- 		warpPerspective(aux_gray,aux_gray,lambda,src_gray.size() );
-    	  
-  		/*Copiando a imagen para a saída da tela*/
-  		aux_gray.copyTo(tela(Rect(0, 0, src_frame.cols, src_frame.rows)));
+        
+        /*Aplicando a perspectiva nos pontos brancos = (275, 159) (461, 162) (477, 315) (262, 322)*/
+        
+        /*Vetores para encontrar a Homography*/
+        vector<Point2f> inputQuad, outputQuad;
+        /*Vetores para reaplicar a Homography e ajustar a imagem na tela*/
+		vector<Point2f> cornerQuad, finalCornerQuad;
+		/*Vetores para encontrar o centro do círculo*/
+		vector<Point2f> center;
+		
+		/*Encontrando a Homography*/
+		/*Pontos para serem modificados*/
+        inputQuad.push_back(Point2f(275, 159));
+        inputQuad.push_back(Point2f(461, 162));
+        inputQuad.push_back(Point2f(477, 315));
+        inputQuad.push_back(Point2f(262, 322));
+        
+        /*Nova localização dos pontos*/
+    	outputQuad.push_back(Point2f(199, 199));
+        outputQuad.push_back(Point2f(301, 202));
+    	outputQuad.push_back(Point2f(302, 298));
+    	outputQuad.push_back(Point2f(199, 302));
+    	
+    	Mat H = findHomography(inputQuad, outputQuad, LMEDS);
+    	
+    	/*Encontrando a nova perspectiva gerada pela Homography*/
+       	cornerQuad.push_back(Point2f(0, 0));
+        cornerQuad.push_back(Point2f(src_gray.cols, 0));
+    	cornerQuad.push_back(Point2f(src_gray.cols, src_gray.rows));
+    	cornerQuad.push_back(Point2f(0, src_gray.rows));
+    	
+    	perspectiveTransform(cornerQuad, finalCornerQuad, H);
+    	
+    	/*Achando os pontos mínimos e máximos de cada eixo para encontrar o tamanho da nova imagem 
+    	e rotaciona-la com uma nova Homography*/
+    	double minx = INF, maxx = -INF, miny = INF, maxy = -INF; 
+    	for (int i=0; i < 4; i++){
+    	  minx = std::min(minx, (double)finalCornerQuad[i].x);
+    	  miny = std::min(miny, (double)finalCornerQuad[i].y);
+    	  maxx = std::max(maxx, (double)finalCornerQuad[i].x);
+    	  maxy = std::max(maxy, (double)finalCornerQuad[i].y);
+    	}
+    	
+    	Size warpSize(maxx - minx, maxy - miny);
+    	
+    	for (int i=0; i < 4; i++){
+    	  finalCornerQuad[i].x -= minx;
+    	  finalCornerQuad[i].y -= miny;
+    	}
+    	
+    	Mat H2 = findHomography(cornerQuad, finalCornerQuad, LMEDS);
+    	
+    	warpPerspective(src_gray, transform, H2, warpSize);
+    	
+    	/*Verificando se é um círculo perfeito*/
+    	centerTrans.push_back(Point2f((double)248.5, (double)248.5));
+    	
+    	Mat invH = findHomography(outputQuad, inputQuad, LMEDS);
+    	
+    	perspectiveTransform(centerTrans, center, invH);
+    	perspectiveTransform(center, centerTrans, H2);
+    
+    	Mat transform2;
+    	cvtColor(transform, transform2, CV_GRAY2RGB);	
+  		cvtColor(transform, transform, CV_GRAY2RGB);
+  		
+    	Scalar color = Scalar(0, 255, 0);   	
+    	circle( transform2, centerTrans[0], 72, color, CV_FILLED, CV_AA, 0);
+    	
+    	/*Adicionando transparencia no círculo*/
+    	double alpha = 0.5;
+    	addWeighted(transform, alpha, transform2, 1.0 - alpha , 0.0, transform2); 
+    	
+    	//imshow( "VideoTool", transform2);
+    	
+        /*Copiando matrix de transformação para out*/
+   		cvtColor(transform, transform, CV_RGB2GRAY);
+   		out = Mat::zeros( src_gray.rows, src_gray.cols, src_gray.type() );
+		transform.copyTo(out);
+ 		    	  
+  		/*Copiando a imagem para a saída da tela*/
+  		//src_gray.copyTo(tela(Rect(0, 0, src_gray.cols, src_gray.rows)));
   		
   		/*Inicializando a função Thresholding*/
  		Threshold( 0, 0 );
@@ -92,22 +175,34 @@ int main(int argc, char** argv) {
     waitKey(0); // key press to close window
 }
 
-void Threshold( int, void* ) {
-  Mat aux(out.rows, out.cols, CV_8U, Scalar(0));
 
-  /*Threshold*/
-  for(int i = 0; i < out.rows; i++) {
-    for(int j = 0; j < out.cols; j++) {
-      if (out.at<uchar>(i, j) < threshold_value) 
-        aux.at<uchar>(i, j) = BLACK;
-      else
-        aux.at<uchar>(i, j) = WHITE;
-    }
-  }
+void Reset( int, void* ) {
+	if(reset == 0 || reset == 1) 
+		src.set(CAP_PROP_POS_MSEC, 380000);
+}
+
+void Threshold( int, void* ) {
+	Mat aux(out.rows, out.cols, CV_8U, Scalar(0));
+	
+	/*Threshold*/
+	cout << "center: (" << centerTrans[0].x << ", " << centerTrans[0].y << ")" << "\n";
+	for(int i = 0; i < out.rows; i++) {
+		for(int j = 0; j < out.cols; j++) {
+			
+			if(((i-centerTrans[0].y)*(i-centerTrans[0].y) + (j-centerTrans[0].x)*(j-centerTrans[0].x)) - 72*72 == 0) { 
+				if (out.at<uchar>(i, j) < threshold_value) 
+					aux.at<uchar>(i, j) = BLACK;
+				else
+					aux.at<uchar>(i, j) = WHITE;
+			}//else
+				//aux.at<uchar>(i, j) = BLACK;
+		}
+	}
   
-  medianBlur(aux, aux, 5);
+	medianBlur(aux, aux, 5);
+	imshow( "VideoTool", aux);
   
-  tracking(aux);
+	//tracking(aux);
 
 }
 
@@ -125,13 +220,13 @@ void tracking(Mat aux) {
   	for(i = 0; i < contours.size(); i++ ){
   	    float area = contourArea(contours[i], false);
   		if (area >= min_area && area <= max_area) {
-  		  //cout << "rato de area: " << area << " indice: " << i << "\n";
+  		  cout << "rato de area: " << area << " indice: " << i << "\n";
   		  break; 
   		}
   	}
   	
   	if (i >= contours.size()){
-  	  //cout << "rato não encontrado\n";
+  	  cout << "rato não encontrado\n";
   	  imshow("VideoTool", tela);
   	  return;
   	}
@@ -146,15 +241,12 @@ void tracking(Mat aux) {
   	
   	//Circulando contornos e destacando centro de massa  
   	Scalar color = Scalar(255,0,0);
-    drawContours( aux, contours, i, color, 2, 8, hierarchy, 0, Point() );
-    circle( aux, mc[i], 4, color, -1, 8, 0 );
-  	
-	/*Copiando a imagen corrigida por ROI*/
-     aux.copyTo(tela(Rect(left1+tela.cols/2, top, aux.cols, aux.rows)));
+    drawContours( out, contours, i, color, 2, 8, hierarchy, 0, Point() );
+    circle( out, mc[i], 4, color, -1, 8, 0 );
   
-	imshow( "VideoTool", tela);
-}
-
-void callbackButton( int, void* ){
-	cout << "hello\n";
+   	/*Copiando a imagen corrigida por ROI*/
+    //aux.copyTo(tela(Rect(left1+tela.cols/2, top, aux.cols, aux.rows))); 
+  	out.copyTo(tela(Rect(tela.cols/2, 0, out.cols, out.rows)));
+ 	 	
+	//imshow( "VideoTool", tela);
 }
