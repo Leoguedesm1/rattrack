c51128468bb05c8a6b046f82e0b71ce21153e518
leoguedes1 <leo_greengm@hotmail.com>
2016-11-03 00:34:12 -0200

    Clearing code and add Radio Tracking Trackbar!

--- videotool.cpp
+++ videotool.cpp
@@ -18,16 +18,19 @@ using namespace std;
 #define WHITE 255
 #define BLACK 0 
 
+/*Estruturas Trackbar*/
+/*Reset*/
+typedef struct {
+	VideoCapture* src;
+}userdatareset;
+
 /*Variáveis Globais*/
 // Imagen 704x480
-Mat src_frame, out, out_perspective, tela, track, invH, H2;
+Mat src_frame, out, out_perspective, tela, track, invH;
 int threshold_value = 100; //Valor do Threshold
 int const max_value = 255; //Valor máximo da Trackbar
 double centerX, centerY;
 int cont = 1;
-
-
-VideoCapture src;
 
 /*Area Bar*/
 int min_area = 0;
@@ -38,45 +41,171 @@ int const max_area_value = 1500;
 int reset = 0;
 int const max_reset = 1;
 
+/*Track Bar*/
+int rtrack = 1;
+int const max_rtrack_value = 5;
+
 /*Assinatura das funções*/
+void acha_perspectiva(VideoCapture src, Mat& transform, Mat& H2, Size warpSize, int FHEIGHT, int FWIDTH);
+void aplica_perspectiva(Mat& transform, Mat& H2, Size warpSize);
+void processa_video(VideoCapture src, Mat& transform, Mat& H2, Size warpSize);
 void Threshold( int, void* );
 void tracking(Mat aux, Mat track);
-void Reset( int, void* );
+void Reset( int, void* udr );
 Mat paint();
+void le_video(VideoCapture& src, char** argv);
+void cria_janela(VideoCapture src, userdatareset& u);
 
 int main(int argc, char** argv) {
-	/*Capturando video*/
-    src.open(argv[1]);
-	src.set(CAP_PROP_POS_MSEC, 380000);
-	
-	/*Verificando se o video foi aberto*/
-    if( !src.isOpened() )
-        throw "Error when reading steam_avi";
-                 
-	/*Janela*/
-    namedWindow( "VideoTool", WINDOW_NORMAL );  	
-    resizeWindow( "VideoTool", 704, 480);
-    namedWindow( "Trackbars", WINDOW_NORMAL);
+    VideoCapture src; //Video de entrada
+ 	
+    /*Le o video de entrada*/
+    le_video(src, argv);
+                     
+    Mat H2, transform = Mat::zeros( src_frame.rows, src_frame.cols, CV_8U ); //H2 = Homography, transform = perspectiva
+    const int FHEIGHT = src.get(CV_CAP_PROP_FRAME_HEIGHT); //FHEIGHT = Altura do video
+    const int FWIDTH = src.get(CV_CAP_PROP_FRAME_WIDTH); //FWIDTH = Largura do video
+ 	Size warpSize(FWIDTH, FHEIGHT); //warpSize = tamanho do warp 
     
-    /*Trackbar reseta o video*/
-    createTrackbar( "Reset", "Trackbars", &reset, max_reset, Reset );
- 	/*Criando a Trackbar para alterar o valor do Thresholding*/
-    createTrackbar( "Threshold", "Trackbars", &threshold_value, max_value, Threshold );
-    /*Trackbars para a area do rato*/
-    createTrackbar( "Area Min", "Trackbars", &min_area, max_area_value, Threshold );
-  	createTrackbar( "Area Max", "Trackbars", &max_area, max_area_value, Threshold );
+    /* Parametro Void* da função Reset*/
+    userdatareset u; 	
+    u.src = &src;
     
-    src >> src_frame;
-    
-    /*Todas as matrizes usadas para transformçãoes*/
-    Mat H, src_gray, transform = Mat::zeros( src_frame.rows, src_frame.cols, CV_8U );
-    cvtColor(src_frame, src_gray, cv::COLOR_RGB2GRAY); //Passando frame do vídeo para GrayScale
-    const int FHEIGHT = src.get(CV_CAP_PROP_FRAME_HEIGHT);
-    const int FWIDTH = src.get(CV_CAP_PROP_FRAME_WIDTH);
  	tela = Mat(FHEIGHT, FWIDTH*2, CV_8UC3, Scalar(0));
   	track = Mat(FHEIGHT, FWIDTH, CV_32S);
+   
+    /*Cria as janelas e os trackbars*/                 
+	cria_janela(src, u);  
+	
+    /*Calcula a Matriz H2*/
+    acha_perspectiva(src, transform, H2, warpSize, FHEIGHT, FWIDTH);
+    
+    /*Aplica a matriz H2 em cada frame do video*/
+    processa_video(src, transform, H2, warpSize);
+   	
+    waitKey(0); // key press to close window
+}
+
+void Reset( int , void* udr) {
+	if(reset == 0 || reset == 1) {
+		userdatareset u = *((userdatareset*)udr); 
+		
+		resizeWindow( "VideoTool", 1000, 1200);
+		
+		cont = 1;
+		track = Scalar(0);
+		
+		u.src->set(CAP_PROP_POS_MSEC, 380000);
+	}
+}
+
+void Threshold( int, void* ) {
+	Mat aux(out_perspective.rows, out_perspective.cols, CV_8U, Scalar(0));
+	
+	resizeWindow( "VideoTool", 1000, 1200);
+	
+	/*Threshold*/
+	for(int i = 0; i < out_perspective.rows; i++) {
+		for(int j = 0; j < out_perspective.cols; j++) {
+			if( ((j-centerX)*(j-centerX)) + ((i-centerY)*(i-centerY)) <= raio*raio) {
+				if (out_perspective.at<uchar>(i, j) < threshold_value) 
+					aux.at<uchar>(i, j) = BLACK;
+				else
+					aux.at<uchar>(i, j) = WHITE;
+			}else
+				aux.at<uchar>(i, j) = BLACK;
+		}
+	}
+  
+	medianBlur(aux, aux, 5);  
+	tracking(aux, track);
+}
+
+void tracking(Mat aux, Mat track) {
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	int i;
+	float area;
+	Mat out_perspective1;
+	
+	resizeWindow( "VideoTool", 1000, 1200);
+	
+	cvtColor(out_perspective, out_perspective1, CV_GRAY2RGB);
+	findContours( aux, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
   	
-  	/*Aplicando a perspectiva nos pontos brancos = (275, 159) (461, 162) (477, 315) (262, 322)*/ 	
+  	for(i = 0; i < contours.size(); i++ ){
+  	    area = contourArea(contours[i], false);
+  		if (area >= min_area && area <= max_area) {
+  		  cout << "rato de area: " << area << " indice: " << i << "\n";
+  		  break; 
+  		}
+  	}
+  	
+  	if (i >= contours.size() || area == 0){
+  	  cout << "rato não encontrado\n";
+  	  imshow("VideoTool", tela);
+  	  return;
+  	}else{
+  		
+  		/// Get the moments
+  		vector<Moments> mu( contours.size() );
+  		mu[i] = moments( contours[i], false ); 
+
+		///  Get the mass centers:
+ 		vector<Point2f> mc( contours.size() );
+  		mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); 
+  	
+  		track.at<int>(mc[i].y, mc[i].x) = cont;
+  		cont++;
+  		Mat p = paint();
+  	
+  		//Circulando contornos e destacando centro de massa  
+    	drawContours( out_perspective1, contours, i, Scalar(255,255,0), 1, 8, hierarchy, 0, Point() );
+    	circle( out_perspective1, mc[i], 2.5, Scalar(0,255,0), -1);
+  	
+		addWeighted(out_perspective1, 1, p, 1, 0.0, out_perspective1); 
+  	  	
+  	  	warpPerspective(out_perspective1, out, invH, src_frame.size());
+  	  	
+   		/*Copiando a imagen corrigida por ROI*/ 
+  		out.copyTo(tela(Rect(0, 0, out.cols, out.rows)));
+  		out_perspective1.copyTo(tela(Rect(out.cols, 0, out_perspective1.cols, out_perspective1.rows)));
+ 	 	
+		imshow( "VideoTool", tela);
+	}
+}
+
+Mat paint() {
+	Mat p = Mat::zeros(track.rows, track.cols, CV_8UC3);
+	Vec3b c1 = Vec3b(255,0,0);
+	Vec3b c2 = Vec3b(0,255,0);
+	for(int i = 0; i < track.rows; i++) {
+		for(int j = 0; j < track.cols; j++) {
+			if(track.at<int>(i, j) != BLACK) {
+				float r = track.at<int>(i,j)/( (float) cont );
+				Vec3b c = (r*c1 + (1-r)*c2);
+				if(rtrack == 0)
+					p.at<Vec3b>(i, j) = c;
+				else {
+					Point a;
+					a.x = j;
+					a.y = i;
+					circle( p, a, rtrack, c, -1);
+				}
+			}
+		}
+	} 
+	return p;
+}
+
+void acha_perspectiva(VideoCapture src, Mat& transform, Mat& H2, Size warpSize, int FHEIGHT, int FWIDTH) {
+	
+	src >> src_frame;
+	
+	Mat H, src_gray;
+	cvtColor(src_frame, src_gray, cv::COLOR_RGB2GRAY); //Passando frame do vídeo para GrayScale
+	 
+	/*Aplicando a perspectiva nos pontos brancos = (275, 159) (461, 162) (477, 315) (262, 322)*/ 		
     /*Vetores para encontrar a Homography*/
     vector<Point2f> inputQuad, outputQuad;
     /*Vetores para reaplicar a Homography e ajustar a imagem na tela*/
@@ -119,8 +248,7 @@ int main(int argc, char** argv) {
     
     int wwidth = maxx - minx;
     int wheight = maxy - miny;
-    Size warpSize(FWIDTH, FHEIGHT);
-    	
+        	
     for (int i=0; i < 4; i++){
       finalCornerQuad[i].x =  (finalCornerQuad[i].x - minx)/( (float) wwidth ) * FWIDTH;
       finalCornerQuad[i].y = (finalCornerQuad[i].y - miny)/( (float) wheight ) * FHEIGHT;
@@ -139,135 +267,60 @@ int main(int argc, char** argv) {
     perspectiveTransform(center, centerTrans, H2);
     centerX = centerTrans[0].x;
     centerY = centerTrans[0].y;  
-    
    
-    /*Loop*/
-    for( ; ; )
-    {
+}
+
+
+void aplica_perspectiva(Mat& transform, Mat& H2, Size warpSize) {
+	Mat src_gray;
+	/*Passando frame do vídeo para GrayScale*/
+    cvtColor(src_frame, src_gray, cv::COLOR_RGB2GRAY); 
+        
+	/*Aplicando transformação de perspectiva*/
+	warpPerspective(src_gray, transform, H2, warpSize);
+        
+	/*Copiando matrix de transformação para out*/
+	out_perspective = Mat::zeros( src_frame.rows, src_frame.cols, CV_8U );
+	transform.copyTo(out_perspective);
+}
+
+void processa_video(VideoCapture src, Mat& transform, Mat& H2, Size warpSize) {
+	/*Loop*/
+    for( ; ; ) {
     	src >> src_frame;
     	
     	if(src_frame.empty())
             break;
-        
-        /*Passando frame do vídeo para GrayScale*/
-        cvtColor(src_frame, src_gray, cv::COLOR_RGB2GRAY); 
-        
-        /*Aplicando transformação de perspectiva*/
-        warpPerspective(src_gray, transform, H2, warpSize);
-        
-        /*Copiando matrix de transformação para out*/
-   		out_perspective = Mat::zeros( src_frame.rows, src_frame.cols, CV_8U );
-		transform.copyTo(out_perspective);
-    
+            
+        aplica_perspectiva(transform, H2, warpSize);
+            
   		/*Inicializando a função Thresholding*/
  		Threshold( 0, 0 );
  		
         waitKey(20); // waits to display frame
     }
-    waitKey(0); // key press to close window
 }
 
-void findPerspective(Mat& H1, Mat& H2, Mat& H3, Mat& H4);
-
-void Reset( int, void* ) {
-	if(reset == 0 || reset == 1) 
-		src.set(CAP_PROP_POS_MSEC, 380000);
-}
-
-void Threshold( int, void* ) {
-	Mat aux(out_perspective.rows, out_perspective.cols, CV_8U, Scalar(0));
+void le_video(VideoCapture& src, char** argv) {
+	src.open(argv[1]);
+	src.set(CAP_PROP_POS_MSEC, 380000);
 	
-	/*Threshold*/
-	for(int i = 0; i < out_perspective.rows; i++) {
-		for(int j = 0; j < out_perspective.cols; j++) {
-			if( ((j-centerX)*(j-centerX)) + ((i-centerY)*(i-centerY)) <= raio*raio) {
-				if (out_perspective.at<uchar>(i, j) < threshold_value) 
-					aux.at<uchar>(i, j) = BLACK;
-				else
-					aux.at<uchar>(i, j) = WHITE;
-			}else
-				aux.at<uchar>(i, j) = BLACK;
-		}
-	}
+	/*Verificando se o video foi aberto*/
+    if( !src.isOpened() )
+        throw "Error when reading steam_avi";
+}
+
+void cria_janela(VideoCapture src, userdatareset& u) {
+	/*Janela*/
+    namedWindow( "VideoTool", WINDOW_NORMAL );  	
   
-	medianBlur(aux, aux, 5);  
-	tracking(aux, track);
-}
-
-void tracking(Mat aux, Mat track) {
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	int i;
-	float area;
-	Mat out_perspective1;
-	
-	cvtColor(out_perspective, out_perspective1, CV_GRAY2RGB);
-	findContours( aux, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-  	
-  	for(i = 0; i < contours.size(); i++ ){
-  	    area = contourArea(contours[i], false);
-  		if (area >= min_area && area <= max_area) {
-  		  cout << "rato de area: " << area << " indice: " << i << "\n";
-  		  break; 
-  		}
-  	}
-  	
-  	if (i >= contours.size() || area == 0){
-  	  cout << "rato não encontrado\n";
-  	  imshow("VideoTool", tela);
-  	  return;
-  	}else{
-  		
-  		/// Get the moments
-  		vector<Moments> mu( contours.size() );
-  		mu[i] = moments( contours[i], false ); 
-
-		///  Get the mass centers:
- 		vector<Point2f> mc( contours.size() );
-  		mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); 
-  	
-  		track.at<int>(mc[i].y, mc[i].x) = cont;
-  		cont++;
-  		Mat p = paint();
-  	
-  		//Circulando contornos e destacando centro de massa  
-    	drawContours( out_perspective1, contours, i, Scalar(255,255,0), 1, 8, hierarchy, 0, Point() );
-    	circle( out_perspective1, mc[i], 2.5, Scalar(0,255,0), -1);
-  	
-  		//cvtColor(track, track, CV_GRAY2RGB);
-		addWeighted(out_perspective1, 1, p, 1, 0.0, out_perspective1); 
-  	  	
-  	  	warpPerspective(out_perspective1, out, invH, src_frame.size());
-  	  	
-   		/*Copiando a imagen corrigida por ROI*/ 
-  		out.copyTo(tela(Rect(0, 0, out.cols, out.rows)));
-  		out_perspective1.copyTo(tela(Rect(out.cols, 0, out_perspective1.cols, out_perspective1.rows)));
- 	 	
-		imshow( "VideoTool", tela);
-	}
-}
-
-Mat paint() {
-	Mat p = Mat::zeros(track.rows, track.cols, CV_8UC3);
-	int color1 = 255;
-	int color2 = 0;
-	Vec3b c1 = Vec3b(255,0,0);
-	Vec3b c2 = Vec3b(0,255,0);
-	for(int i = 0; i < track.rows; i++) {
-		for(int j = 0; j < track.cols; j++) {
-			if(track.at<int>(i, j) != BLACK) {
-				/*
-				color1 = color1 - 255*track.at<int>(i, j)/ ((float) cont);
-				color2 = color2 + (track.at<int>(i, j)/ ((float) cont) )*255;
-				p.at<Vec3b>(i, j).val[0] = color1;
-				p.at<Vec3b>(i, j).val[1] = color2;
-				p.at<Vec3b>(i, j).val[2] = 0;
-				*/
-				float r = track.at<int>(i,j)/( (float) cont );
-				Vec3b c = (r*c1 + (1-r)*c2);
-				p.at<Vec3b>(i, j) = c;
-			}
-		}
-	} 
-	return p;
+    /*Trackbar reseta o video*/
+    createTrackbar( "Reset", "VideoTool", &reset, max_reset, Reset, (void*) (&u) );
+ 	/*Criando a Trackbar para alterar o valor do Thresholding*/
+    createTrackbar( "Threshold", "VideoTool", &threshold_value, max_value, Threshold );
+    /*Trackbars para a area do rato*/
+    createTrackbar( "Area Min", "VideoTool", &min_area, max_area_value, Threshold );    
+  	createTrackbar( "Area Max", "VideoTool", &max_area, max_area_value, Threshold );
+  	/*Tracbars raio tracking*/
+  	createTrackbar( "Raio Track", "VideoTool", &rtrack, max_rtrack_value, Threshold );
 }
