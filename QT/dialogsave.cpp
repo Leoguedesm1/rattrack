45277e1f7e6656ac72da7d64984eeb6ef1b85e5e
Leonardo Guedes Moreira <leo_greengm@hotmail.com>
2017-02-02 20:48:26 +0000

    Replace dialogsave.cpp

--- QT/dialogsave.cpp
+++ QT/dialogsave.cpp
@@ -26,44 +26,68 @@ DialogSave::~DialogSave()
 void DialogSave::on_btOK_clicked()
 {
 
-    QString dirOriginal, dir;
+    QString dirTela = "", dirOriginal = "", dirPerspectiva = "", dirPath = "";
     string save;
 
 
-    if(ui->cbTela->isChecked() != true && ui->cbOriginal->isChecked() != true && ui->cbPerspectiva->isChecked() != true) {
+    if(ui->cbTela->isChecked() != true && ui->cbOriginal->isChecked() != true && ui->cbPerspectiva->isChecked() != true && ui->cbPath->isChecked() != true) {
         QMessageBox::critical(this, tr ("Erro"), tr ("Escolha uma das opcoes."));
     }else {
-        dirOriginal = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
-                                                    "/home",
-                                                    QFileDialog::ShowDirsOnly
-                                                    | QFileDialog::DontResolveSymlinks);
+
+        if(ui->cbTela->isChecked() == true) {
+            stringstream save1;
+            save1 << "Tela_" << animal << "_Teste" << teste << ".bmp";
+            save = save1.str();
+            dirTela = QFileDialog::getSaveFileName(this, tr("Salvar Teste"), QString::fromStdString(save), tr("Image File (*.bmp);;All Files (*)"));
+
+            if(dirTela != "") {
+                dirTela = dirTela.toUtf8().data();
+                imwrite(dirTela.toAscii().data(), tela);
+                QMessageBox::information(this, tr ("Salvar"), tr("Arquivo salvo com sucesso!"));
+            }
+        }
+
+        if(ui->cbOriginal->isChecked() == true) {
+            stringstream save1;
+            save1 << "Original_" << animal << "_Teste" << teste << ".bmp";
+            save = save1.str();
+            dirOriginal = QFileDialog::getSaveFileName(this, tr("Salvar Teste"), QString::fromStdString(save), tr("Image File (*.bmp);;All Files (*)"));
+
+            if(dirOriginal != "") {
+                dirOriginal = dirOriginal.toUtf8().data();
+                imwrite(dirOriginal.toAscii().data(), out);
+                QMessageBox::information(this, tr ("Salvar"), tr("Arquivo salvo com sucesso!"));
+            }
+        }
+
+        if(ui->cbPerspectiva->isChecked() == true) {
+            stringstream save1;
+            save1 << "Perspectiva_" << animal << "_Teste" << teste << ".bmp";
+            save = save1.str();
+            dirPerspectiva = QFileDialog::getSaveFileName(this, tr("Salvar Teste"), QString::fromStdString(save), tr("Image File (*.bmp);;All Files (*)"));
+
+            if(dirPerspectiva != "") {
+                dirPerspectiva = dirPerspectiva.toUtf8().data();
+                imwrite(dirPerspectiva.toAscii().data(), out);
+                QMessageBox::information(this, tr ("Salvar"), tr("Arquivo salvo com sucesso!"));
+            }
+        }
+
+        if(ui->cbPath->isChecked() == true) {
+            stringstream save1;
+            save1 << "Caminho_" << animal << "_Teste" << teste << ".bmp";
+            save = save1.str();
+            dirPath = QFileDialog::getSaveFileName(this, tr("Salvar Teste"), QString::fromStdString(save), tr("Image File (*.bmp);;All Files (*)"));
+
+            if(dirPath != "") {
+                dirPath = dirPath.toUtf8().data();
+                imwrite(dirPath.toAscii().data(), p);
+                QMessageBox::information(this, tr ("Salvar"), tr("Arquivo salvo com sucesso!"));
+            }
+        }
+
+        this->close();
     }
-
-    dir = dirOriginal.toUtf8().data();
-
-    if(ui->cbTela->isChecked() == true) {
-        stringstream save1;
-        save1 << dir.toAscii().data() << "/Tela_" << animal << "_Teste" << teste << ".bmp";
-        save = save1.str();
-        imwrite(save, tela);
-    }
-
-    if(ui->cbOriginal->isChecked() == true) {
-        stringstream save1;
-        save1 << dir.toAscii().data() << "/Original_" << animal << "_Teste" << teste << ".bmp";
-        save = save1.str();
-        imwrite(save, out);
-    }
-
-    if(ui->cbPerspectiva->isChecked() == true) {
-        stringstream save1;
-        save1 << dir.toAscii().data() << "/Perspectiva_" << animal << "_Teste" << teste << ".bmp";
-        save = save1.str();
-        imwrite(save, out_original);
-    }
-
-    QMessageBox::information(this, tr ("Salvar"), tr("Arquivos salvos com sucesso!"));
-    this->close();
 }
 
 void DialogSave::on_btCancel_clicked()
