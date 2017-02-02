32577f204580619d2a1ac0c6de88791a3f80d9ee
Leonardo Guedes Moreira <leo_greengm@hotmail.com>
2017-02-02 20:46:56 +0000

    Replace controlpainel.cpp

--- QT/controlpainel.cpp
+++ QT/controlpainel.cpp
@@ -15,7 +15,7 @@ double raio = 112;
 VideoCapture src;
 userdatareset u;
 
-Mat src_frame, out, out_perspective, tela, track, invH, out_original, transform_mat;
+Mat src_frame, out, out_perspective, tela, track, invH, out_original, transform_mat, p;
 double centerX, centerY;
 int cont = 1;
 QTimer *tmrTimer;
@@ -120,7 +120,7 @@ void controlpainel::tracking(Mat aux, Mat track) {
         addWeighted(out_perspective1, 1, p, 1, 0.0, out_perspective1);
         warpPerspective(out_perspective1, out, invH, src_frame.size());
         out_original = out_perspective1;
-        mostra_tela(out_perspective1);
+        mostra_tela(p);
 
         return;
     }else{
@@ -135,7 +135,7 @@ void controlpainel::tracking(Mat aux, Mat track) {
 
         track.at<int>(mc[i].y, mc[i].x) = cont;
         cont++;
-        Mat p = paint();
+        p = paint();
 
         //Circulando contornos e destacando centro de massa
         drawContours( out_perspective1, contours, i, Scalar(255,255,0), 1, 8, hierarchy, 0, Point() );
@@ -144,13 +144,13 @@ void controlpainel::tracking(Mat aux, Mat track) {
         warpPerspective(out_perspective1, out, invH, src_frame.size());
 
         out_original = out_perspective1;
-        mostra_tela(out_perspective1);
+        mostra_tela(p);
 
     }
 }
 
 Mat controlpainel::paint() {
-    Mat p = Mat::zeros(track.rows, track.cols, CV_8UC3);
+    Mat painting = Mat::zeros(track.rows, track.cols, CV_8UC3);
     Vec3b c1 = Vec3b(255,0,0);
     Vec3b c2 = Vec3b(0,255,0);
     for(int i = 0; i < track.rows; i++) {
@@ -159,17 +159,17 @@ Mat controlpainel::paint() {
                 float r = track.at<int>(i,j)/( (float) cont );
                 Vec3b c = (r*c1 + (1-r)*c2);
                 if(rtrack == 0)
-                    p.at<Vec3b>(i, j) = c;
+                    painting.at<Vec3b>(i, j) = c;
                 else {
                     Point a;
                     a.x = j;
                     a.y = i;
-                    circle( p, a, rtrack, c, -1);
+                    circle( painting, a, rtrack, c, -1);
                 }
             }
         }
     }
-    return p;
+    return painting;
 }
 
 void controlpainel::acha_perspectiva(Mat& transform_mat, Mat& H2, Size warpSize, int FHEIGHT, int FWIDTH) {
