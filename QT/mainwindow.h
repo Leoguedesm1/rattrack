97fcff24ac129ff0d1423af8e86c9d008ef81cae
Leonardo Guedes Moreira <leo_greengm@hotmail.com>
2017-03-08 05:54:44 +0000

    Replace mainwindow.h

--- QT/mainwindow.h
+++ QT/mainwindow.h
@@ -1,14 +1,22 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+//Bibliotecas QT
 #include <QMainWindow>
-#include <QString>
+#include <QLineEdit>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QTimer>
+#include <QAction>
+#include <QMenu>
 
+//Bibliotecas C
 #include <stdio.h>
 #include <algorithm>
 #include <iostream>
 #include <limits>
 
+//Bibliotecas OPENCV
 #include <highgui.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -17,8 +25,8 @@
 #include <opencv2/opencv.hpp>
 #include "opencv/cv.h"
 
-using namespace cv;
 using namespace std;
+using namespace cv;
 
 namespace Ui {
 class MainWindow;
@@ -28,26 +36,71 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public slots:
-    void openControlPainel();
-
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-
 private slots:
-    void on_rbCam_clicked();
-    void on_rbFile_clicked();
-    void on_bSelectFile_clicked();
-    void on_bOK_clicked();
-    void le_video_file(VideoCapture& src, string argv);
-    void le_video_cam(VideoCapture& src, string argv);
-    void all_reset();
+
+    //GUI functions
+    void on_btFile_clicked();
+    void on_btIniciar_clicked();
+    void on_btReset_clicked();
+    void on_btPlay_clicked();
+
+    void create_buttonTela();
+    void activePerspective();
+    void activeOriginal();
+
+    void on_tbThresh_valueChanged(int value);
+    void on_tbMin_valueChanged(int value);
+    void on_tbMax_valueChanged(int value);
+    void on_tbTrack_valueChanged(int value);
+
+    //Rattrack functions
+    void le_video_file(VideoCapture& src, QString argv);
+    void reset_video();
+    void playorpause_video();
+    void rattrack();
+    void acha_perspectiva(Mat& transform_image, Mat& H2_warp, Size warpSize, int FHEIGHT, int FWIDTH);
+    void aplica_perspectiva();
+    void processa_video();
+    void Threshold();
+    void tracking(Mat aux, Mat track_image);
+    Mat paint();
+    void mostrar_imagem(Mat& frame);
+    void resetar_variaveis();
+
 
 private:
+
+    //GUI variables
     Ui::MainWindow *ui;
-   };
+    QLineEdit *animalInput;
+    QLineEdit *testeInput;
+    QAction *showPerspective;
+    QAction *showOriginal;
+    QMenu *tela_menu;
+    int original, perspective;
+
+    QString animal;
+    QString teste;
+    QString argv;
+
+    //Rattrack variables
+    VideoCapture src;
+    double fps;
+    int total_frames;
+    QTimer *tmrTimer;
+
+    Mat src_frame, perspective_frame, original_frame, tela_image;
+    Mat track_image, transform_image, p;
+    Mat H2_warp, invH_warp;
+
+    Size warpSize;
+    int FHEIGHT, FWIDTH, centerX, centerY, threshold_value, min_area, max_area, coordX, coordY, rtrack;
+    unsigned int cont_track;
+
+};
 
 #endif // MAINWINDOW_H
-
