8d982860fd110252c1c4fee3ac26adafdd315664
Leonardo Guedes Moreira <leo_greengm@hotmail.com>
2017-04-06 23:33:58 +0000

    Replace mainwindow.h

--- QT/mainwindow.h
+++ QT/mainwindow.h
@@ -9,19 +9,26 @@
 #include <QTimer>
 #include <QAction>
 #include <QMenu>
+#include <QInputDialog>
 
 //Bibliotecas C
 #include <stdio.h>
 #include <algorithm>
 #include <iostream>
 #include <limits>
+#include <string.h>
+#include <time.h>
+#include <cctype>
 
 //Bibliotecas OPENCV
 #include <highgui.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/core/utility.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/videoio.hpp"
 #include <opencv2/opencv.hpp>
 #include "opencv/cv.h"
 
@@ -58,9 +65,13 @@ private slots:
     void on_tbMax_valueChanged(int value);
     void on_tbTrack_valueChanged(int value);
 
-    void setFileName(int contFiles);
+    void setFileName(double contFiles);
     void reset_interface();
     void inputNameTeste();
+
+    void on_btCamConfig_clicked();
+    void on_btSair_clicked();
+
 
     //Rattrack functions
     void le_video_file(VideoCapture& src, QString argv);
@@ -74,12 +85,62 @@ private slots:
     void Threshold();
     void tracking(Mat aux, Mat track_image);
     Mat paint();
-    void mostrar_imagem(Mat& frame);
+    void mostrar_imagem(Mat frame);
     void resetar_variaveis();
     void encerra_video();
     void salvar_imagens();
     Mat gera_IA();
 
+    //Calibration functions
+    void captureImagesCalibration(
+        int* numSquares, int* numCornersHor,
+        int* numCornersVer, int* numCorners,
+        Size* board_sz, vector<Point2f> corners);
+
+    double computeReprojectionErrors(
+            const vector<vector<Point3f> >& objectPoints,
+            const vector<vector<Point2f> >& imagePoints,
+            const vector<Mat>& rvecs, const vector<Mat>& tvecs,
+            const Mat& cameraMatrix, const Mat& distCoeffs,
+            vector<float>& perViewErrors );
+
+    void calcChessboardCorners(
+        Size boardSize,
+        float squareSize,
+        vector<Point3f>& corners);
+
+    bool runCalibration(
+        vector<vector<Point2f> > imagePoints,
+        Size imageSize, Size boardSize,
+        float squareSize, float aspectRatio,
+        int flags, Mat& cameraMatrix, Mat& distCoeffs,
+        vector<Mat>& rvecs, vector<Mat>& tvecs,
+        vector<float>& reprojErrs, double& totalAvgErr);
+
+    void saveCameraParams(
+        const string& filename, Size imageSize, Size boardSize,
+        float squareSize, float aspectRatio, int flags,
+        const Mat& cameraMatrix, const Mat& distCoeffs,
+        const vector<Mat>& rvecs, const vector<Mat>& tvecs,
+        const vector<float>& reprojErrs,
+        const vector<vector<Point2f> >& imagePoints,
+        double totalAvgErr );
+
+    bool readStringList(
+        const string& filename, vector<string>& l );
+
+    bool runAndSave(
+        const string& outputFilename,
+        const vector<vector<Point2f> >& imagePoints,
+        Size imageSize, Size boardSize, float squareSize,
+        float aspectRatio, int flags, Mat& cameraMatrix,
+        Mat& distCoeffs, bool writeExtrinsics, bool writePoints );
+
+    bool getCalibrationCamera(Size* boardSize, vector<Point2f> corners, string getErro);
+
+    bool getHomographyMatrix(Size *board_sz, int* numCornersHor, int* numCornersVer, int* numCorners, vector<Point2f> corners, string getErro);
+
+    void setCalibrationCamera();
 
 private:
 
@@ -99,7 +160,7 @@ private:
     QDir directory;
 
     int numberFiles;
-    int contFiles;
+    double contFiles;
 
     //Rattrack variables
     VideoCapture src;
