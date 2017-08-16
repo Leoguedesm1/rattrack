ec164ca81e227de3ecee649bcee2697e4e710593
Leonardo <leonardo@git@gitlab.com:leoguedes1/rattrack.git>
2017-08-15 23:43:22 -0300

    Add interface for read files!

--- src/readerinterface.h
+++ src/readerinterface.h
@@ -1,5 +1,27 @@
 #ifndef READERINTERFACE
 #define READERINTERFACE
 
+#include <iostream>
+#include <cv.h>
+
+using namespace std;
+using namespace cv;
+
+class ReaderInterface {
+
+public:
+
+    virtual void readFile(const string& path) = 0;
+
+    //Over written
+    virtual void getInFile(const char* name, Mat* data) = 0;
+    virtual void getInFile(const char* name, Point2d* data) = 0;
+    virtual void getInFile(const char* name, double* data) = 0;
+
+    virtual void closeFile() = 0;
+
+    virtual bool exists(const string& path) = 0;
+};
+
 #endif // READERINTERFACE
 
--- src/readerxml.h
+++ src/readerxml.h
@@ -1,5 +1,45 @@
 #ifndef READERXML
 #define READERXML
 
+#include "readerinterface.h"
+
+class ReaderXml :  public ReaderInterface {
+
+public:
+    void readFile(const string& path) {
+        file = FileStorage(path, FileStorage::READ);
+    }
+
+    void getInFile(const char* name, Mat* data) {
+        file[name] >> (*data);
+    }
+
+    void getInFile(const char *name, Point2d *data) {
+        file[name] >> (*data);
+    }
+
+    void getInFile(const char *name, double *data) {
+        file[name] >> (*data);
+    }
+
+    void closeFile() {
+        file.release();
+    }
+
+    bool exists(const string& path) {
+        if(file.isOpened())
+            return true;
+        else {
+            readFile(path);
+            if(file.isOpened())
+                return true;
+        }
+        return false;
+    }
+
+private:
+    FileStorage file;
+};
+
 #endif // READERXML
 
